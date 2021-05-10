11c428926c23cb11495027a5d013de22a6b71394
JeusT0910 <72457937+JeusT0910@users.noreply.github.com>
2021-05-10 23:05:29 +0700

    commit1

---
+++ database/seeds/nha_san_xuats.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class nha_san_xuats extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
