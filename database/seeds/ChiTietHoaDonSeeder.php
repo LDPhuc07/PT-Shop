1724b84ce4a3ffb2027abc9a9412eb9c7b9de8f6
LDPhuc07 <74141717+LDPhuc07@users.noreply.github.com>
2021-11-01 12:05:42 +0700

    commit seed

--- database/seeds/ChiTietHoaDonSeeder.php
+++ database/seeds/ChiTietHoaDonSeeder.php
@@ -11,7 +11,97 @@ class ChiTietHoaDonSeeder extends Seeder
      */
     public function run()
     {
-       
+        DB::table('chi_tiet_hoa_dons')->insert([
+            ['id'=> 1, 'chi_tiet_san_phams_id'=> 41,'hoa_dons_id'=>1, 'gia_goc'=>20000, 'gia_ban'=>25000, 'so_luong'=>'1'],
+            ['id'=> 2, 'chi_tiet_san_phams_id'=> 60,'hoa_dons_id'=>2, 'gia_goc'=>100000, 'gia_ban'=>108000,'so_luong'=>'2'],
+            ['id'=> 3, 'chi_tiet_san_phams_id'=> 10,'hoa_dons_id'=>3, 'gia_goc'=>7000000, 'gia_ban'=>8000000,'so_luong'=>'1'],
+            ['id'=> 4, 'chi_tiet_san_phams_id'=> 60,'hoa_dons_id'=>4, 'gia_goc'=>100000, 'gia_ban'=>108000,'so_luong'=>'3'],
+            ['id'=> 5, 'chi_tiet_san_phams_id'=> 16,'hoa_dons_id'=>5, 'gia_goc'=>6000000, 'gia_ban'=>7000000,'so_luong'=>'1'],
+            ['id'=> 6, 'chi_tiet_san_phams_id'=> 32,'hoa_dons_id'=>6, 'gia_goc'=>4000000, 'gia_ban'=>4050000,'so_luong'=>'1'],
+            ['id'=> 7, 'chi_tiet_san_phams_id'=> 43,'hoa_dons_id'=>7, 'gia_goc'=>1000000, 'gia_ban'=>1275000,'so_luong'=>'2'],
+            ['id'=> 8, 'chi_tiet_san_phams_id'=> 45,'hoa_dons_id'=>8, 'gia_goc'=>1000000, 'gia_ban'=>1275000,'so_luong'=>'2'],
+            ['id'=> 9, 'chi_tiet_san_phams_id'=> 52,'hoa_dons_id'=>9, 'gia_goc'=>1000000, 'gia_ban'=>1500000,'so_luong'=>'1'],
+            ['id'=> 10, 'chi_tiet_san_phams_id'=> 19,'hoa_dons_id'=>10, 'gia_goc'=>8000000, 'gia_ban'=>9000000,'so_luong'=>'1'],
+            ['id'=> 12, 'chi_tiet_san_phams_id'=> 39,'hoa_dons_id'=>11, 'gia_goc'=>20000, 'gia_ban'=>25000,'so_luong'=>'5'],
+            ['id'=> 13, 'chi_tiet_san_phams_id'=> 16,'hoa_dons_id'=>12, 'gia_goc'=>6000000, 'gia_ban'=>7000000,'so_luong'=>'1'],
+            ['id'=> 14, 'chi_tiet_san_phams_id'=> 47,'hoa_dons_id'=>13, 'gia_goc'=>100000, 'gia_ban'=>160000,'so_luong'=>'3'],
+            ['id'=> 15, 'chi_tiet_san_phams_id'=> 17,'hoa_dons_id'=>14, 'gia_goc'=>6000000, 'gia_ban'=>7000000,'so_luong'=>'2'],
+            ['id'=> 16, 'chi_tiet_san_phams_id'=> 57,'hoa_dons_id'=>15, 'gia_goc'=>200000, 'gia_ban'=>250000,'so_luong'=>'3'],
+            ['id'=> 17, 'chi_tiet_san_phams_id'=> 37,'hoa_dons_id'=>16, 'gia_goc'=>20000, 'gia_ban'=>25000,'so_luong'=>'6'],
+            ['id'=> 18, 'chi_tiet_san_phams_id'=> 28,'hoa_dons_id'=>17, 'gia_goc'=>1000000, 'gia_ban'=>1200000,'so_luong'=>'2'],
+            ['id'=> 19, 'chi_tiet_san_phams_id'=> 48,'hoa_dons_id'=>18, 'gia_goc'=>100000, 'gia_ban'=>160000,'so_luong'=>'4'],
+            ['id'=> 20, 'chi_tiet_san_phams_id'=> 10,'hoa_dons_id'=>19, 'gia_goc'=>7000000, 'gia_ban'=>8000000,'so_luong'=>'1'],
+            ['id'=> 21, 'chi_tiet_san_phams_id'=> 10,'hoa_dons_id'=>20, 'gia_goc'=>7000000, 'gia_ban'=>8000000,'so_luong'=>'1'],
+            ['id'=> 22, 'chi_tiet_san_phams_id'=> 25,'hoa_dons_id'=>21, 'gia_goc'=>1200000, 'gia_ban'=>1600000,'so_luong'=>'1'],
+            ['id'=> 23, 'chi_tiet_san_phams_id'=> 16,'hoa_dons_id'=>22, 'gia_goc'=>6000000, 'gia_ban'=>7000000,'so_luong'=>'1'],
+            
+            ['id'=> 24, 'chi_tiet_san_phams_id'=> 56,'hoa_dons_id'=>23, 'gia_goc'=>200000, 'gia_ban'=>250000,'so_luong'=>'3'],
+            ['id'=> 25, 'chi_tiet_san_phams_id'=> 46,'hoa_dons_id'=>24, 'gia_goc'=>1000000, 'gia_ban'=>1275000,'so_luong'=>'1'],
+            ['id'=> 26, 'chi_tiet_san_phams_id'=> 48,'hoa_dons_id'=>25, 'gia_goc'=>100000, 'gia_ban'=>160000,'so_luong'=>'2'],
+
+            ['id'=> 27, 'chi_tiet_san_phams_id'=> 39,'hoa_dons_id'=>26, 'gia_goc'=>20000, 'gia_ban'=>25000,'so_luong'=>'5'],
+            ['id'=> 28, 'chi_tiet_san_phams_id'=> 9,'hoa_dons_id'=>27, 'gia_goc'=>1000000, 'gia_ban'=>1200000,'so_luong'=>'3'],
+            ['id'=> 29, 'chi_tiet_san_phams_id'=> 7,'hoa_dons_id'=>28, 'gia_goc'=>1000000, 'gia_ban'=>1200000,'so_luong'=>'1'],
+            ['id'=> 30, 'chi_tiet_san_phams_id'=> 5,'hoa_dons_id'=>29, 'gia_goc'=>4000000, 'gia_ban'=>5000000,'so_luong'=>'1'],
+
+            ['id'=> 31, 'chi_tiet_san_phams_id'=> 59,'hoa_dons_id'=>30, 'gia_goc'=>100000, 'gia_ban'=>108000,'so_luong'=>'3'],
+            ['id'=> 32, 'chi_tiet_san_phams_id'=> 25,'hoa_dons_id'=>31, 'gia_goc'=>1200000, 'gia_ban'=>1600000,'so_luong'=>'2'],
+
+            ['id'=> 33, 'chi_tiet_san_phams_id'=> 6,'hoa_dons_id'=>32, 'gia_goc'=>4000000, 'gia_ban'=>5000000,'so_luong'=>'2'],
+            ['id'=> 34, 'chi_tiet_san_phams_id'=> 4,'hoa_dons_id'=>33, 'gia_goc'=>2000000, 'gia_ban'=>2250000,'so_luong'=>'1'],
+            ['id'=> 35, 'chi_tiet_san_phams_id'=> 6,'hoa_dons_id'=>34, 'gia_goc'=>4000000, 'gia_ban'=>5000000,'so_luong'=>'2'],
+            ['id'=> 36, 'chi_tiet_san_phams_id'=> 10,'hoa_dons_id'=>35, 'gia_goc'=>7000000, 'gia_ban'=>8000000,'so_luong'=>'2'],
+            ['id'=> 37, 'chi_tiet_san_phams_id'=> 26,'hoa_dons_id'=>36, 'gia_goc'=>1200000, 'gia_ban'=>1600000,'so_luong'=>'1'],
+            ['id'=> 38, 'chi_tiet_san_phams_id'=> 6,'hoa_dons_id'=>37, 'gia_goc'=>4000000, 'gia_ban'=>5000000,'so_luong'=>'1'],
+            ['id'=> 39, 'chi_tiet_san_phams_id'=> 13,'hoa_dons_id'=>38, 'gia_goc'=>2000000, 'gia_ban'=>2400000,'so_luong'=>''],
+            ['id'=> 40, 'chi_tiet_san_phams_id'=> 4,'hoa_dons_id'=>39, 'gia_goc'=>2000000, 'gia_ban'=>2000000,'so_luong'=>'2'],
+            ['id'=> 41, 'chi_tiet_san_phams_id'=> 14,'hoa_dons_id'=>40, 'gia_goc'=>6000000, 'gia_ban'=>7000000, 'so_luong'=>'2'],
+
+            ['id'=> 42, 'chi_tiet_san_phams_id'=> 41,'hoa_dons_id'=>51, 'gia_goc'=>20000, 'gia_ban'=>25000, 'so_luong'=>'1'],
+            ['id'=> 43, 'chi_tiet_san_phams_id'=> 60,'hoa_dons_id'=>52, 'gia_goc'=>100000, 'gia_ban'=>108000,'so_luong'=>'2'],
+            ['id'=> 44, 'chi_tiet_san_phams_id'=> 10,'hoa_dons_id'=>53, 'gia_goc'=>7000000, 'gia_ban'=>8000000,'so_luong'=>'1'],
+            ['id'=> 45, 'chi_tiet_san_phams_id'=> 60,'hoa_dons_id'=>54, 'gia_goc'=>100000, 'gia_ban'=>108000,'so_luong'=>'3'],
+            ['id'=> 46, 'chi_tiet_san_phams_id'=> 16,'hoa_dons_id'=>55, 'gia_goc'=>6000000, 'gia_ban'=>7000000,'so_luong'=>'1'],
+            ['id'=> 47, 'chi_tiet_san_phams_id'=> 32,'hoa_dons_id'=>56, 'gia_goc'=>4000000, 'gia_ban'=>4050000,'so_luong'=>'1'],
+            ['id'=> 48, 'chi_tiet_san_phams_id'=> 43,'hoa_dons_id'=>57, 'gia_goc'=>1000000, 'gia_ban'=>1275000,'so_luong'=>'2'],
+            ['id'=> 49, 'chi_tiet_san_phams_id'=> 45,'hoa_dons_id'=>28, 'gia_goc'=>1000000, 'gia_ban'=>1275000,'so_luong'=>'2'],
+            ['id'=> 50, 'chi_tiet_san_phams_id'=> 52,'hoa_dons_id'=>29, 'gia_goc'=>1000000, 'gia_ban'=>1500000,'so_luong'=>'1'],
+            ['id'=> 51, 'chi_tiet_san_phams_id'=> 19,'hoa_dons_id'=>30, 'gia_goc'=>8000000, 'gia_ban'=>9000000,'so_luong'=>'1'],
+            ['id'=> 52, 'chi_tiet_san_phams_id'=> 39,'hoa_dons_id'=>31, 'gia_goc'=>20000, 'gia_ban'=>25000,'so_luong'=>'5'],
+            ['id'=> 53, 'chi_tiet_san_phams_id'=> 16,'hoa_dons_id'=>32, 'gia_goc'=>6000000, 'gia_ban'=>7000000,'so_luong'=>'1'],
+            ['id'=> 54, 'chi_tiet_san_phams_id'=> 47,'hoa_dons_id'=>33, 'gia_goc'=>100000, 'gia_ban'=>160000,'so_luong'=>'3'],
+            ['id'=> 55, 'chi_tiet_san_phams_id'=> 17,'hoa_dons_id'=>34, 'gia_goc'=>6000000, 'gia_ban'=>7000000,'so_luong'=>'2'],
+            ['id'=> 56, 'chi_tiet_san_phams_id'=> 57,'hoa_dons_id'=>35, 'gia_goc'=>200000, 'gia_ban'=>250000,'so_luong'=>'3'],
+            ['id'=> 57, 'chi_tiet_san_phams_id'=> 37,'hoa_dons_id'=>36, 'gia_goc'=>20000, 'gia_ban'=>25000,'so_luong'=>'6'],
+            ['id'=> 58, 'chi_tiet_san_phams_id'=> 28,'hoa_dons_id'=>37, 'gia_goc'=>1000000, 'gia_ban'=>1200000,'so_luong'=>'2'],
+            ['id'=> 59, 'chi_tiet_san_phams_id'=> 48,'hoa_dons_id'=>38, 'gia_goc'=>100000, 'gia_ban'=>160000,'so_luong'=>'4'],
+            ['id'=> 60, 'chi_tiet_san_phams_id'=> 10,'hoa_dons_id'=>39, 'gia_goc'=>7000000, 'gia_ban'=>8000000,'so_luong'=>'1'],
+            ['id'=> 61, 'chi_tiet_san_phams_id'=> 10,'hoa_dons_id'=>40, 'gia_goc'=>7000000, 'gia_ban'=>8000000,'so_luong'=>'1'],
+            ['id'=> 62, 'chi_tiet_san_phams_id'=> 25,'hoa_dons_id'=>41, 'gia_goc'=>1200000, 'gia_ban'=>1600000,'so_luong'=>'1'],
+            ['id'=> 63, 'chi_tiet_san_phams_id'=> 16,'hoa_dons_id'=>52, 'gia_goc'=>6000000, 'gia_ban'=>7000000,'so_luong'=>'1'],
+            
+            ['id'=> 64, 'chi_tiet_san_phams_id'=> 56,'hoa_dons_id'=>53, 'gia_goc'=>200000, 'gia_ban'=>250000,'so_luong'=>'3'],
+            ['id'=> 65, 'chi_tiet_san_phams_id'=> 46,'hoa_dons_id'=>54, 'gia_goc'=>1000000, 'gia_ban'=>1275000,'so_luong'=>'1'],
+            ['id'=> 66, 'chi_tiet_san_phams_id'=> 48,'hoa_dons_id'=>55, 'gia_goc'=>100000, 'gia_ban'=>160000,'so_luong'=>'2'],
+
+            ['id'=> 67, 'chi_tiet_san_phams_id'=> 39,'hoa_dons_id'=>56, 'gia_goc'=>20000, 'gia_ban'=>25000,'so_luong'=>'5'],
+            ['id'=> 68, 'chi_tiet_san_phams_id'=> 9,'hoa_dons_id'=>57, 'gia_goc'=>1000000, 'gia_ban'=>1200000,'so_luong'=>'3'],
+            ['id'=> 69, 'chi_tiet_san_phams_id'=> 7,'hoa_dons_id'=>28, 'gia_goc'=>1000000, 'gia_ban'=>1200000,'so_luong'=>'1'],
+            ['id'=> 70, 'chi_tiet_san_phams_id'=> 5,'hoa_dons_id'=>29, 'gia_goc'=>4000000, 'gia_ban'=>5000000,'so_luong'=>'1'],
+
+            ['id'=> 71, 'chi_tiet_san_phams_id'=> 59,'hoa_dons_id'=>30, 'gia_goc'=>100000, 'gia_ban'=>108000,'so_luong'=>'3'],
+            ['id'=> 72, 'chi_tiet_san_phams_id'=> 25,'hoa_dons_id'=>31, 'gia_goc'=>1200000, 'gia_ban'=>1600000,'so_luong'=>'2'],
+
+            ['id'=> 73, 'chi_tiet_san_phams_id'=> 6,'hoa_dons_id'=>32, 'gia_goc'=>4000000, 'gia_ban'=>5000000,'so_luong'=>'2'],
+            ['id'=> 74, 'chi_tiet_san_phams_id'=> 4,'hoa_dons_id'=>33, 'gia_goc'=>2000000, 'gia_ban'=>2250000,'so_luong'=>'1'],
+            ['id'=> 75, 'chi_tiet_san_phams_id'=> 6,'hoa_dons_id'=>34, 'gia_goc'=>4000000, 'gia_ban'=>5000000,'so_luong'=>'2'],
+            ['id'=> 76, 'chi_tiet_san_phams_id'=> 10,'hoa_dons_id'=>35, 'gia_goc'=>7000000, 'gia_ban'=>8000000,'so_luong'=>'2'],
+            ['id'=> 77, 'chi_tiet_san_phams_id'=> 26,'hoa_dons_id'=>36, 'gia_goc'=>1200000, 'gia_ban'=>1600000,'so_luong'=>'1'],
+            ['id'=> 78, 'chi_tiet_san_phams_id'=> 6,'hoa_dons_id'=>37, 'gia_goc'=>4000000, 'gia_ban'=>5000000,'so_luong'=>'1'],
+            ['id'=> 79, 'chi_tiet_san_phams_id'=> 13,'hoa_dons_id'=>38, 'gia_goc'=>2000000, 'gia_ban'=>2400000,'so_luong'=>''],
+            ['id'=> 80, 'chi_tiet_san_phams_id'=> 4,'hoa_dons_id'=>39, 'gia_goc'=>2000000, 'gia_ban'=>2000000,'so_luong'=>'2'],
+            ['id'=> 81, 'chi_tiet_san_phams_id'=> 14,'hoa_dons_id'=>40, 'gia_goc'=>6000000, 'gia_ban'=>7000000, 'so_luong'=>'2'],
+        ]);
     }
 
 }
--- database/seeds/HoaDonSeeder.php
+++ database/seeds/HoaDonSeeder.php
@@ -13,7 +13,78 @@ class HoaDonSeeder extends Seeder
     public function run()
     {
         DB::table('hoa_dons')->insert([
+            ['id'=> 1, 'tai_khoans_id'=> 4,'ngay_lap_hd'=>'2021-01-02', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 2, 'tai_khoans_id'=> 6,'ngay_lap_hd'=>'2021-02-03', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 3, 'tai_khoans_id'=> 10,'ngay_lap_hd'=>'2021-03-04', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>true],
+            ['id'=> 4, 'tai_khoans_id'=> 6,'ngay_lap_hd'=>'2021-04-04', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>true],
+            ['id'=> 5, 'tai_khoans_id'=> 6,'ngay_lap_hd'=>'2021-05-30', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>true],
+            ['id'=> 6, 'tai_khoans_id'=> 3,'ngay_lap_hd'=>'2021-06-23', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 7, 'tai_khoans_id'=> 4,'ngay_lap_hd'=>'2021-07-05', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 8, 'tai_khoans_id'=> 4,'ngay_lap_hd'=>'2021-08-03', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 9, 'tai_khoans_id'=> 5,'ngay_lap_hd'=>'2021-09-21', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 10, 'tai_khoans_id'=> 9,'ngay_lap_hd'=>'2021-10-12', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>true],
+            ['id'=> 12, 'tai_khoans_id'=> 9,'ngay_lap_hd'=>'2021-11-08', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 13, 'tai_khoans_id'=> 6,'ngay_lap_hd'=>'2021-01-01', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 14, 'tai_khoans_id'=> 7,'ngay_lap_hd'=>'2021-02-17', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 15, 'tai_khoans_id'=> 7,'ngay_lap_hd'=>'2021-03-15', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 16, 'tai_khoans_id'=> 7,'ngay_lap_hd'=>'2021-04-15', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 17, 'tai_khoans_id'=> 7,'ngay_lap_hd'=>'2021-05-16', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 18, 'tai_khoans_id'=> 8,'ngay_lap_hd'=>'2021-06-20', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 19, 'tai_khoans_id'=> 8,'ngay_lap_hd'=>'2021-07-01', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 20, 'tai_khoans_id'=> 10,'ngay_lap_hd'=>'2021-08-09', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 21, 'tai_khoans_id'=> 10,'ngay_lap_hd'=>'2021-09-29', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 22, 'tai_khoans_id'=> 5,'ngay_lap_hd'=>'2021-10-06', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>true],
+            ['id'=> 23, 'tai_khoans_id'=> 6,'ngay_lap_hd'=>'2021-11-01', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>true],
             
+            ['id'=> 24, 'tai_khoans_id'=> 6,'ngay_lap_hd'=>'2021-01-25', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>true],
+            ['id'=> 25, 'tai_khoans_id'=> 4,'ngay_lap_hd'=>'2021-02-20', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 26, 'tai_khoans_id'=> 8,'ngay_lap_hd'=>'2021-10-24', 'chot_don'=>'l', 'hinh_thuc_thanh_toan'=>false],
+
+            ['id'=> 27, 'tai_khoans_id'=> 9,'ngay_lap_hd'=>'2021-10-25', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>true],
+            ['id'=> 28, 'tai_khoans_id'=> 9,'ngay_lap_hd'=>'2021-10-26', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 29, 'tai_khoans_id'=> 7,'ngay_lap_hd'=>'2021-10-27', 'chot_don'=>'l', 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 30, 'tai_khoans_id'=> 5,'ngay_lap_hd'=>'2021-10-28', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false],
+
+            ['id'=> 31, 'tai_khoans_id'=> 9,'ngay_lap_hd'=>'2021-10-29', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 32, 'tai_khoans_id'=> 5,'ngay_lap_hd'=>'2021-10-30', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false],
+
+            ['id'=> 33, 'tai_khoans_id'=> 6,'ngay_lap_hd'=>'2021-10-31', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 34, 'tai_khoans_id'=> 4,'ngay_lap_hd'=>'2021-11-01', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 35, 'tai_khoans_id'=> 6,'ngay_lap_hd'=>'2021-11-02', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 36, 'tai_khoans_id'=> 10,'ngay_lap_hd'=>'2021-11-03', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>true],
+            ['id'=> 37, 'tai_khoans_id'=> 6,'ngay_lap_hd'=>'2021-11-04', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>true],
+            ['id'=> 38, 'tai_khoans_id'=> 6,'ngay_lap_hd'=>'2021-11-05', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>true],
+            ['id'=> 39, 'tai_khoans_id'=> 3,'ngay_lap_hd'=>'2021-11-06', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 40, 'tai_khoans_id'=> 4,'ngay_lap_hd'=>'2021-11-07', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 41, 'tai_khoans_id'=> 4,'ngay_lap_hd'=>'2021-11-08', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false],
+            // ['id'=> 42, 'tai_khoans_id'=> 5,'ngay_lap_hd'=>'2021-11-09', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>false],
+            // ['id'=> 43, 'tai_khoans_id'=> 9,'ngay_lap_hd'=>'2021-11-10', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>true],
+            // ['id'=> 44, 'tai_khoans_id'=> 9,'ngay_lap_hd'=>'2021-11-11', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false],
+            // ['id'=> 45, 'tai_khoans_id'=> 6,'ngay_lap_hd'=>'2021-11-12', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>false],
+            // ['id'=> 46, 'tai_khoans_id'=> 7,'ngay_lap_hd'=>'2021-11-13', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>false],
+            // ['id'=> 47, 'tai_khoans_id'=> 7,'ngay_lap_hd'=>'2021-11-14', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false],
+            // ['id'=> 48, 'tai_khoans_id'=> 7,'ngay_lap_hd'=>'2021-11-15', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false],
+            // ['id'=> 49, 'tai_khoans_id'=> 7,'ngay_lap_hd'=>'2021-11-16', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false],
+            // ['id'=> 50, 'tai_khoans_id'=> 8,'ngay_lap_hd'=>'2021-11-17', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 51, 'tai_khoans_id'=> 8,'ngay_lap_hd'=>'2021-11-01', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 52, 'tai_khoans_id'=> 10,'ngay_lap_hd'=>'2021-11-02', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 53, 'tai_khoans_id'=> 10,'ngay_lap_hd'=>'2021-11-03', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>false],
+            ['id'=> 54, 'tai_khoans_id'=> 5,'ngay_lap_hd'=>'2021-11-04', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>true],
+            ['id'=> 55, 'tai_khoans_id'=> 6,'ngay_lap_hd'=>'2021-11-05', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>true],
+            
+            ['id'=> 56, 'tai_khoans_id'=> 6,'ngay_lap_hd'=>'2021-11-06', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>true],
+            ['id'=> 57, 'tai_khoans_id'=> 4,'ngay_lap_hd'=>'2021-11-07', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false],
+            // ['id'=> 58, 'tai_khoans_id'=> 8,'ngay_lap_hd'=>'2021-11-08', 'chot_don'=>'l', 'hinh_thuc_thanh_toan'=>false],
+
+            // ['id'=> 59, 'tai_khoans_id'=> 9,'ngay_lap_hd'=>'2021-11-09', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>true],
+            // ['id'=> 60, 'tai_khoans_id'=> 9,'ngay_lap_hd'=>'2021-11-10', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>false],
+            // ['id'=> 61, 'tai_khoans_id'=> 7,'ngay_lap_hd'=>'2021-11-11', 'chot_don'=>'l', 'hinh_thuc_thanh_toan'=>false],
+            // ['id'=> 62, 'tai_khoans_id'=> 5,'ngay_lap_hd'=>'2021-11-12', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false],
+
+            // ['id'=> 63, 'tai_khoans_id'=> 9,'ngay_lap_hd'=>'2021-11-13', 'chot_don'=>true, 'hinh_thuc_thanh_toan'=>false],
+            // ['id'=> 64, 'tai_khoans_id'=> 5,'ngay_lap_hd'=>'2021-11-14', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false],
+
+            // ['id'=> 65, 'tai_khoans_id'=> 6,'ngay_lap_hd'=>'2021-11-15', 'chot_don'=>false, 'hinh_thuc_thanh_toan'=>false]
         ]);
     }
 }
