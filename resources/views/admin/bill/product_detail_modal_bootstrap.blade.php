7bb7da386b11727f9d67d7dadb412b29aee9c0a6
JeusT0910 <72457937+JeusT0910@users.noreply.github.com>
2021-11-11 13:49:56 +0700

    commit

--- resources/views/admin/bill/product_detail_modal_bootstrap.blade.php
+++ resources/views/admin/bill/product_detail_modal_bootstrap.blade.php
@@ -1,7 +1,7 @@
 <table style="min-width: unset" class="table-ds-sanpham">
     <thead>
         <tr>
-            <th>Mã hóa đơn</th>
+            {{-- <th>Mã hóa đơn</th> --}}
             <th>Ảnh</th>
             <th>Tên Sản Phẩm</th>
             <th>Màu</th>
@@ -14,7 +14,7 @@
     <tbody>
       @foreach($arrays as $bill_detail)
       <tr>
-          <td>{{ $bill_detail->hoa_dons_id}}</td>
+          {{-- <td>{{ $bill_detail->hoa_dons_id}}</td> --}}
           <td>@foreach($bill_detail->chiTietSanPham->sanPham->anh as $anh)
             @if($anh->anhchinh == 1)
               <img src="{{asset($anh->link)}}" alt="anh">
@@ -23,9 +23,9 @@
           <td>{{ $bill_detail->chiTietSanPham->sanPham->ten_san_pham}}</td>
           <td>{{ $bill_detail->chiTietSanPham->mau }}</td>
           <td>{{ $bill_detail->chiTietSanPham->kich_thuoc }}</td>
-          <td>{{number_format(($bill_detail->chiTietSanPham->sanPham->gia_ban*(100-$bill_detail->chiTietSanPham->sanPham->giam_gia)/100),0,',','.').' '.'VNĐ'}}</td>
+          <td>{{number_format($bill_detail->gia_ban,0,',','.').' '.'VNĐ'}}</td>
           <td>{{ $bill_detail->so_luong}}</td>
-          <td>{{number_format(($bill_detail->chiTietSanPham->sanPham->gia_ban*(100-$bill_detail->chiTietSanPham->sanPham->giam_gia)/100) * $bill_detail->so_luong,0,',','.').' '.'VNĐ'}}</td>
+          <td>{{number_format($bill_detail->gia_ban * $bill_detail->so_luong,0,',','.').' '.'VNĐ'}}</td>
       </tr>
       @endforeach
   </tbody>
--- resources/views/pages/my_bill_detail.blade.php
+++ resources/views/pages/my_bill_detail.blade.php
@@ -4,35 +4,112 @@
             <div>Phí ship: 30.000 VNĐ</div>
             <div>Thành Tiền: 3.000.000 VNĐ</div>
           </div>  --}}
-          <form action="">
-            <div class="my-order-heading">
-              <div class="row" style="text-align:center">
-                <div class="col-4">Sản phẩm</div>
-                <div class="col-1">Số lượng</div>
-                <div class="col-3">Giá</div>
-                <div class="col-1">Giảm giá</div>
-                <div class="col-3">Thành tiền</div>
-              </div>
-            </div>
-            @foreach($array as $bill_detail)
-                <div class="body-two">
-                    <div class="row" style="text-align:center; margin-top:10px">
-                        <div class="col-4" style="display: flex;">
-                        @foreach($bill_detail->chiTietSanPham->sanPham->anh as $anh)
+       
+<style>
+  .table-ds-sanpham {
+    width: 100%;
+  }
+  .table-ds-sanpham tbody tr {
+    background-color: #fafafa;
+  }
+
+  .table-ds-sanpham > thead > tr > th:first-of-type, .table-ds-sanpham > tbody > tr > td:first-of-type {
+    padding-left: 24px;
+  }
+  .table-ds-sanpham > thead > tr > th, .table-ds-sanpham > tbody > tr > td {
+    padding: 12px;
+    font-size: 13px;
+  }
+
+  .table-ds-sanpham thead th {
+    border-bottom: 1px solid #dee2e6;
+    border-top: 1px solid #dee2e6;
+    font-weight: 600;
+    }
+
+    .table-ds-sanpham tbody td img {
+      height: 35px;
+      width: 35px;
+      object-fit: cover;
+    }
+    .modal-body {
+      padding: unset;
+    }
+    @media (min-width: 576px){
+      .modal-dialog {
+        max-width: 700px;
+      }
+    }
+</style>
+
+            {{-- <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th>Sản phẩm</th>
+                  <th>Màu</th>
+                  <th>Kích thước</th>
+                  <th>Giá</th>
+                  <th>Số lượng</th>
+                  <th>Thành tiền</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach($array as $bill_detail)
+                  <tr>
+                    <td>
+                      @foreach($bill_detail->chiTietSanPham->sanPham->anh as $anh)
                         <img src="{{asset($anh->link)}}" alt="" style="width: 50px;height: 50px;margin-right: 5px;">
-                        @endforeach
-                          <div>
-                            <h5>{{ $bill_detail->chiTietSanPham->sanPham->ten_san_pham }}</h5>
-                            <span>Màu: {{ $bill_detail->chiTietSanPham->mau }}</span>
-                            <br/>
-                            <span>Kích thước: {{ $bill_detail->chiTietSanPham->kich_thuoc }}</span>
-                          </div>
-                        </div>
-                        <div class="col-1">{{ $bill_detail->so_luong }}</div>
-                        <div class="col-3">{{number_format($bill_detail->chiTietSanPham->sanPham->gia_ban,0,',','.').' '.'VNĐ'}}</div>
-                        <div class="col-1">{{ $bill_detail->chiTietSanPham->sanPham->giam_gia }}%</div>
-                        <div class="col-3">{{number_format(($bill_detail->chiTietSanPham->sanPham->gia_ban*(100-$bill_detail->chiTietSanPham->sanPham->giam_gia)/100) * $bill_detail->so_luong,0,',','.').' '.'VNĐ'}}</div>
-                    </div>
-                </div>
-            @endforeach
-          </form>
+                      @endforeach
+                      <h5>{{ $bill_detail->chiTietSanPham->sanPham->ten_san_pham }}</h5>
+                    </td>
+                    <td>
+                      {{ $bill_detail->chiTietSanPham->mau }}
+                    </td>
+                    <td>
+                      {{ $bill_detail->chiTietSanPham->kich_thuoc }}
+                    </td>
+                    <td>
+                      {{number_format($bill_detail->gia_ban,0,',','.').' '.'VNĐ'}}
+                    </td>
+                    <td>
+                      {{ $bill_detail->so_luong }}
+                    </td>
+                    <td>
+                      {{number_format($bill_detail->gia_ban * $bill_detail->so_luong,0,',','.').' '.'VNĐ'}}
+                    </td>
+                  </tr>
+                @endforeach
+              </tbody>
+            </table> --}}
+            <table style="min-width: unset" class="table-ds-sanpham"  >
+              <thead>
+                  <tr>
+                      {{-- <th>Mã hóa đơn</th> --}}
+                      <th>Ảnh</th>
+                      <th>Tên Sản Phẩm</th>
+                      <th>Màu</th>
+                      <th>Kích cỡ</th>
+                      <th>Giá bán</th>
+                      <th>Số lượng</th>
+                      <th>Thành tiền</th>
+                  </tr>
+              </thead>
+              <tbody>
+                @foreach($array as $bill_detail)
+                <tr>
+                    {{-- <td>{{ $bill_detail->hoa_dons_id}}</td> --}}
+                    <td>@foreach($bill_detail->chiTietSanPham->sanPham->anh as $anh)
+                      @if($anh->anhchinh == 1)
+                        <img src="{{asset($anh->link)}}" alt="anh">
+                      @endif
+                    @endforeach</td>
+                    <td>{{ $bill_detail->chiTietSanPham->sanPham->ten_san_pham}}</td>
+                    <td>{{ $bill_detail->chiTietSanPham->mau }}</td>
+                    <td>{{ $bill_detail->chiTietSanPham->kich_thuoc }}</td>
+                    <td>{{number_format($bill_detail->gia_ban,0,',','.').' '.'VNĐ'}}</td>
+                    <td>{{ $bill_detail->so_luong}}</td>
+                    <td>{{number_format($bill_detail->gia_ban * $bill_detail->so_luong,0,',','.').' '.'VNĐ'}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+          </table>
